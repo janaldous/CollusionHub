35ee49230b0ab6eff557780762c50680e327be5f
Shubham Goel <shubham145@hotmail.com>
2015-02-28 21:45:36 +0000

    Template
    homepage
    profilepage
    eventpage
    tutorpage
    lostfoundpage
    sharingpage

---
+++ Collusionhubhtml/Templates/CollusionHubTemp.dwt.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<style type="text/css">
+body {
+	background-image: url(../imeet-london-background.png);
+}
+</style>
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<script type="text/javascript">
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+
+<body onLoad="MM_preloadImages('../MenuButtons/Home2.png','../MenuButtons/Profile2.png','../MenuButtons/tutors2.png','../MenuButtons/events2.png','../MenuButtons/sharing2.png','../MenuButtons/L&amp;F2.png','../MenuButtons/logout2.png')">
+<div>
+  <div align="center">
+    <table width="899" border="0">
+      <tbody>
+        <tr>
+          <td width="255"><img src="../logowithwomen.png" width="250" height="149" alt=""/></td>
+          <td width="634">&nbsp;</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+<div>
+  <div align="center">
+    <table width="1000" border="0">
+      <tbody>
+        <tr>
+          <td width="156"><p><a href="../HomePage.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('HomeButton','','../MenuButtons/Home2.png',1)"><img src="../MenuButtons/Home.png" alt="" width="148" height="52" id="HomeButton"></a></p>
+          <p><a href="../ProfilePage.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('ProfileButton','','../MenuButtons/Profile2.png',1)"><img src="../MenuButtons/Profile.png" alt="" width="148" height="52" id="ProfileButton"></a></p>
+          <p><a href="../TutorPage.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('TutorButton','','../MenuButtons/tutors2.png',1)"><img src="../MenuButtons/tutors.png" alt="" width="148" height="52" id="TutorButton"></a></p>
+          <p><a href="../EventPage.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('EventButton','','../MenuButtons/events2.png',1)"><img src="../MenuButtons/events.png" alt="" width="148" height="52" id="EventButton"></a></p>
+          <p><a href="../SharingIsCaring.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('SICButton','','../MenuButtons/sharing2.png',1)"><img src="../MenuButtons/sharing.png" alt="" width="148" height="52" id="SICButton"></a></p>
+          <p><a href="../LostFoundPage.php" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('L&FButton','','../MenuButtons/L&F2.png',1)"><img src="../MenuButtons/L&F.png" alt="" width="148" height="52" id="L&amp;FButton"></a></p>
+          <p><a href="#" onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('LogOutButton','','../MenuButtons/logout2.png',1)"><img src="../MenuButtons/logout.png" alt="" width="148" height="52" id="LogOutButton"></a></p></td>
+          <td width="709"><table width="708" border="0">
+            <tbody>
+              <tr>
+                <td width="702"><!-- TemplateBeginEditable name="PublicFeed" -->PublicFeed<!-- TemplateEndEditable --></td>
+              </tr>
+              <tr>
+                <td>
+                  <!-- TemplateBeginEditable name="PrivateFeed" -->PrivateFeed<!-- TemplateEndEditable -->
+              </tbody>
+          </table></td>
+          <td width="121"><!-- TemplateBeginEditable name="WidgetPanel" -->WidgetPanel<!-- TemplateEndEditable --></td>
+        </tr>
+      </tbody>
+    </table>
+    All right reserved by The A-Team </div>
+</div>
+</body>
+</html>
