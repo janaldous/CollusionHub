458be195b680b20c729088a073c4572e91ccbc1e
Iman Merzaie <imanfkl@gmail.com>
2015-02-28 18:20:54 +0000

    insert to db

--- inserttodb.php
+++ inserttodb.php
@@ -1,6 +1,13 @@
 <?php
     //receive information sent by post method
-    //
+//    $username = $_POST[username];
+//    $password = $_POST[password];
+//    $firstname = $_POST[firstname];
+//    $lastname = $_POST[lastname];
+//    $uni $_POST[university];
+//    $email $_POST[email];
+//    $newstudent $_POST[newstudent];
+
     $username = $_POST[username];
     $password = $_POST[password];
     $firstname = $_POST[firstname];
@@ -18,9 +25,8 @@
     //select database
     mysql_select_db("unification");
     //insert values
-    $insert_query = "INSERT INTO missing (firstname, lastname, age, gender, height, distinguishingfeatures, mothername, fathername, lastseenplace, lastseentime, lost, contact) VALUES ('$firstname', '$lastname', '$age', '$gender', '$height', '$distinguishingfeatures', '$mothername', '$fathername', '$lastseenplace', '$lastseentime', '$lost' , '$contact')";
 
-    $insert_query = "INSERT INTO missing (username, password, firstname, lastname, email, university, newstudent) VALUES ('$username','$password','$firstname','$lastname','$uni','$email','$newstudent')";
+    $insert_query = "INSERT INTO missing (username, password, firstname, lastname, email, university, newstudent) VALUES ('$username','$password','$firstname','$lastname','$email','$uni','$newstudent')";
     mysql_query($insert_query);
     //close MySQL
     mysql_close($sql);
