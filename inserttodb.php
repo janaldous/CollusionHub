e7607b1728f79eeac14d761cf154e27c6ecfe4b6
Iman Merzaie <imanfkl@gmail.com>
2015-02-28 18:09:30 +0000

    has adding query to db

---
+++ inserttodb.php
@@ -0,0 +1,27 @@
+<?php
+    //receive information sent by post method
+    //
+    $username = $_POST[username];
+    $password = $_POST[password];
+    $firstname = $_POST[firstname];
+    $lastname = $_POST[lastname];
+    $uni $_POST[university];
+    $email $_POST[email];
+    $newstudent $_POST[newstudent];
+    
+    //connect to MySQL
+    $servername = "mysql12.000webhost.com";
+    $username = "a1331560_shubham";
+    $password = "Increed123";
+    $sql = mysql_connect($servername,$username,$password);
+    mysql_connect($servername,$username,$password);
+    //select database
+    mysql_select_db("unification");
+    //insert values
+    $insert_query = "INSERT INTO missing (firstname, lastname, age, gender, height, distinguishingfeatures, mothername, fathername, lastseenplace, lastseentime, lost, contact) VALUES ('$firstname', '$lastname', '$age', '$gender', '$height', '$distinguishingfeatures', '$mothername', '$fathername', '$lastseenplace', '$lastseentime', '$lost' , '$contact')";
+
+    $insert_query = "INSERT INTO missing (username, password, firstname, lastname, email, university, newstudent) VALUES ('$username','$password','$firstname','$lastname','$uni','$email','$newstudent')";
+    mysql_query($insert_query);
+    //close MySQL
+    mysql_close($sql);
+?>
