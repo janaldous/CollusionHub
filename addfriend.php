a02c0e39deab130dd38e882fac0dc9c52545ab94
janaldous <jat.torres@gmail.com>
2015-02-28 21:38:54 +0000

    add friend script done, needs testing

--- addfriend.php
+++ addfriend.php
@@ -2,19 +2,17 @@
     session_start(); // Starting Session
 	$error='ERROR'; // Variable To Store Error Message
 	
-	// Define 
-	$username = $_POST[username];
-    $password = $_POST[password];
-    $firstname = $_POST[firstname];
-    $lastname = $_POST[lastname];
-    $university = $_POST[university];
-    $email =  $_POST[email];
+	// Define
+
+	$id = $_POST[id];
+	$friend = $_POST[friend];
+    $tutor = $_POST[tutor];
 	// Establishing Connection with Server by passing server_name, user_id and password as a parameter
 	$connection = mysql_connect("mysql12.000webhost.com", "a1331560_shubham", "Increed123")  or die("1");
 	// Selecting Database
 	$db = mysql_select_db("a1331560_account", $connection)  or die("2");
 	// SQL query to fetch information of registerd users and finds user match.
-	$insert_query = "INSERT INTO `a1331560_account`.`students` (`username`, `password`, `firstname`, `lastname`, `email`, `university`, `newstudent`) VALUES ('$username', '$password', '$firstname', '$lastname', '$university', '$email', '1')";
+	$insert_query = "INSERT INTO `a1331560_account`.`friendtutors` (`id`, `friends`, `tutors`) VALUES ('$id', '$friends', '$tutors')";
 	mysql_query($insert_query, $connection) or die("3");
 	mysql_close($connection); // Closing Connection
 ?>
